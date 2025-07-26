47c5ccb084d8636f45fcee61407e680146406e80
Avinash B B (Accenture International Limited) <v-avinashbb@microsoft.com>
2025-07-26 20:05:45 +0530

    sandbox issue

--- tests/myTest.js
+++ tests/myTest.js
@@ -3,11 +3,15 @@ const assert = require('assert');
 
 describe('Google Homepage', function () {
   it('should have the correct title', async function () {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'] // <== Fix here
+    });
+
     const page = await browser.newPage();
     await page.goto('https://www.google.com');
     const title = await page.title();
-    assert.strictEqual(title.includes('Google'), true);
+    assert.ok(title.includes('Google'));
     await browser.close();
   });
 });
